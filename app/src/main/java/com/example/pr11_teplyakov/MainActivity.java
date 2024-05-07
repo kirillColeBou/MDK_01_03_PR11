2108a487b2c5a714b247e5d6beb9d05791715132
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-08 01:37:32 +0500

    Реализован интерфейс. Тепляков

--- app/src/main/java/com/example/pr11_teplyakov/MainActivity.java
+++ app/src/main/java/com/example/pr11_teplyakov/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
